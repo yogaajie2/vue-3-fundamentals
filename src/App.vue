3044b32dda8485af65d98e3239d0a8799e4a84d2
yogaajie2 <yogaajie2@gmail.com>
2022-03-08 10:49:01 +0700

    first commit + lesson 3: List Rendering

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const header = ref('Shopping List App');
+const items = ref([
+  { id: 1, label: '10 party hats' },
+  { id: 2, label: '2 board games' },
+  { id: 3, label: '20 cups' },
+]);
+</script>
+
+<template>
+  <h1>{{ header }}</h1>
+
+  <ul>
+    <li
+      v-for="({id, label}, index) in items"
+      :key="id"
+    >
+      {{ label }}
+    </li>
+  </ul>
+</template>
