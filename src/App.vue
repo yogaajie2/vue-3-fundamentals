19970c27708c3f41853cdddd325a5e191e7ffcc1
yogaajie2 <yogaajie2@gmail.com>
2022-03-08 11:33:34 +0700

    Lesson 4: User Inputs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const header = ref('Shopping List App');
+const header = ref("Shopping List App");
+
 const items = ref([
-  { id: 1, label: '10 party hats' },
-  { id: 2, label: '2 board games' },
-  { id: 3, label: '20 cups' },
+  { id: 1, label: "10 party hats" },
+  { id: 2, label: "2 board games" },
+  { id: 3, label: "20 cups" },
 ]);
+
+const newItem = ref("");
+const newItemHighPriority = ref(false);
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+
+  <input
+    v-model.trim="newItem"
+    type="text"
+    placeholder="Add an item"
+  >
+
+  <label>
+    <input
+      v-model="newItemHighPriority"
+      type="checkbox"
+    >
+
+    High Priority
+  </label>
 
   <ul>
     <li
