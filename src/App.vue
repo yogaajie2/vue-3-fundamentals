edae5fea19f9a6e7c8b2a1d88fbbd083010b250b
yogaajie2 <yogaajie2@gmail.com>
2022-03-08 11:55:23 +0700

    Lesson 6: Methods

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,33 @@ import { ref } from "vue";
 const header = ref("Shopping List App");
 
 const items = ref([
-  { id: 1, label: "10 party hats" },
-  { id: 2, label: "2 board games" },
-  { id: 3, label: "20 cups" },
+  {
+    id: 1,
+    label: "10 party hats",
+  },
+
+  {
+    id: 2,
+    label: "2 board games",
+  },
+
+  {
+    id: 3,
+    label: "20 cups",
+  },
 ]);
 
 const newItem = ref("");
 const newItemHighPriority = ref(false);
+
+const saveItem = () => {
+  items.value.push({
+    id: items.value.length + 1,
+    label: newItem.value,
+  });
+
+  newItem.value = "";
+};
 </script>
 
 <template>
@@ -18,7 +38,7 @@ const newItemHighPriority = ref(false);
 
   <form
     class="add-item-form"
-    @submit.prevent="items.push({ id:items.length + 1, label: newItem })"
+    @submit.prevent="saveItem"
   >
     <input
       v-model.trim="newItem"
